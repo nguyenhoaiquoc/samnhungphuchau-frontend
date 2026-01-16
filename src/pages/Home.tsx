a28174535cc3bfc96ccf8b0cb29dce35b5027118
Nguyễn Hoài Quốc <nguyenhoaiquoc5@gmail.com>
2026-01-16 22:18:50 +0700

    init frontend

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-green-600">
+        Đây là trang Home 🌿
+      </h1>
+    </div>
+  )
+}
+
+export default Home
